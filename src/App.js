9a118880c6d22b4ce391dc92e46a2dac8dd906f8
AyushVerma150 <64376745+AyushVerma150@users.noreply.github.com>
2021-06-07 21:35:29 +0530

    New navigation style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+const App = () => {
+  const [currentForm, setCurrentForm] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Grid
+        container
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          style={{
+            height: "60px",
+            marginTop: "0",
+          }}
+          item
+          xs={6}
+          sm={4}
         >
-          Learn React
-        </a>
-      </header>
+          <Paper
+            onClick={() => {
+              setCurrentForm(0);
+            }}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "0",
+              backgroundColor: currentForm === 0 ? "lightgray" : "white",
+              borderBottom: currentForm === 0 ? "4px solid black" : "none",
+            }}
+          >
+            <h3>Business Profile</h3>
+          </Paper>
+        </Grid>
+        <Grid
+          style={{
+            height: "60px",
+            marginTop: "0",
+          }}
+          item
+          xs={6}
+          sm={4}
+        >
+          <Paper
+            onClick={() => {
+              setCurrentForm(1);
+            }}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: currentForm === 1 ? "lightgray" : "white",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "0",
+              borderBottom: currentForm === 1 ? "4px solid black" : "none",
+            }}
+          >
+            <h3>Banking details</h3>
+          </Paper>
+        </Grid>
+        <Grid
+          style={{
+            height: "60px",
+            marginTop: "0",
+          }}
+          item
+          xs={6}
+          sm={4}
+        >
+          <Paper
+            onClick={() => {
+              setCurrentForm(2);
+            }}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: currentForm === 2 ? "lightgray" : "white",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "0",
+              outline: "none",
+              borderBottom: currentForm === 2 ? "4px solid black" : "none",
+            }}
+          >
+            <h3>Identity Proof</h3>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
